48d0f78c4b0503b7b0cafe0775c3f4b43af4d70d
Chris Celaya <mr.christophercelaya@gmail.com>
2023-01-09 00:58:41 -0700

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,26 +1,55 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is";
+const basePromptPrefix = `
+Write me a detailed table of contents for a blog post with the title below.
+
+Title:
+`;
+
 const generateAction = async (req, res) => {
-  // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+	console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
-  const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.8,
-    max_tokens: 250,
-  });
-  
-  const basePromptOutput = baseCompletion.data.choices.pop();
+	const baseCompletion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+		temperature: 0.8,
+		max_tokens: 250,
+	});
 
-  res.status(200).json({ output: basePromptOutput });
+	const basePromptOutput = baseCompletion.data.choices.pop();
+
+	// I build Prompt #2.
+	const secondPrompt = `
+ Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+
+ Title: ${req.body.userInput}
+
+ Table of Contents: ${basePromptOutput.text}
+
+ Blog Post:
+ `;
+
+	// I call the OpenAI API a second time with Prompt #2
+	const secondPromptCompletion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: `${secondPrompt}`,
+		// I set a higher temperature for this one. Up to you!
+		temperature: 0.85,
+		// I also increase max_tokens.
+		max_tokens: 1250,
+	});
+
+	// Get the output
+	const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+	// Send over the Prompt #2's output to our UI instead of Prompt #1's.
+	res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
